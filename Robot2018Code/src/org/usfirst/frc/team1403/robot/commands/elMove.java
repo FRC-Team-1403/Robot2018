48d2f8c07949f3340a3d4428c486ded451f957a3
CougarRoboticsProg1 <cougarroboticsprog1@gmail.com>
2018-02-16 20:35:09 -0500

    Commit cuarenta - real end of day 2/16/2018

--- Robot2018Code/src/org/usfirst/frc/team1403/robot/commands/elMove.java
+++ Robot2018Code/src/org/usfirst/frc/team1403/robot/commands/elMove.java
@@ -2,6 +2,8 @@ package org.usfirst.frc.team1403.robot.commands;
 
 import org.usfirst.frc.team1403.robot.Robot;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.command.Command;
 
@@ -29,12 +31,14 @@ public class elMove extends Command {
     	
     	switch(spot)
     	{
-    	case 2: direction = true; 
-    			gate = Robot.elevator.opticSwitch; 
-    			break;
-    	case 1: direction = false; 
-    			gate = Robot.elevator.opticIntake;
-    			break; 
+    	case 2:
+    		direction = true; 
+    		gate = Robot.elevator.opticSwitch; 
+    		break;
+    	case 1:
+    		direction = false; 
+    		gate = Robot.elevator.opticIntake;
+    		break; 
     	}
     	
     	
@@ -52,9 +56,13 @@ public class elMove extends Command {
     }
 
     // Called once after isFinished returns true
-    protected void end() { }
+    protected void end() { 
+    	Robot.elevator.elMotor.set(ControlMode.PercentOutput, 0);
+    }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
-    protected void interrupted() { }
+    protected void interrupted() {
+    	Robot.elevator.elMotor.set(ControlMode.PercentOutput, 0);
+    }
 }
